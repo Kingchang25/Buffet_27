ee93e407cb72320946b2891ecf0136cf42c3fa80
Kingchang25 <lachang5711@gmail.com>
2026-09-03 18:44:01 +0000

    update

--- Java/000_Labs/003_output/base_code/starter.java
+++ Java/000_Labs/003_output/base_code/starter.java
@@ -1,6 +1,6 @@
 /*
- *	Author:  
- *  Date: 
+ *	Author:  Luke Castillote Chang
+ *  Date: 9/2/26
 */
 
 import java.util.Scanner;
@@ -9,6 +9,10 @@ class starter {
 	public static void main(String args[]) {
 		// the string "I love to learn coding remotely." will appear in
 		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		System.out.println("Happy Birthday ,To you."); 
+		System.out.println("Happy Birthday, To you.");
+		System.out.println("Happy Birthday dear Goel,");
+		System.out.println("Happy Birthday, to you.");
+
 	}
 }
--- Java/000_Labs/004_String_class/base_code/starter.java
+++ Java/000_Labs/004_String_class/base_code/starter.java
@@ -1,14 +1,17 @@
 /*
- *	Author:  
- *  Date: 
+ *	Author:  Luke Castillote Chang
+ *  Date: 9/2/26
 */
 
 import java.util.Scanner;
 
 class starter {
 	public static void main(String args[]) {
-		// the string "I love to learn coding remotely." will appear in
-		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		String ara = new String ("Happy Birthday, to you");
+		System.out.println(ara); 
+		String axper = new String("Happy Birthday dear Goel Bozmalyan");
+		System.out.println(axper); 
+		String bozit = new String ("Happy Birthday, to you");
+		System.out.print(bozit); 
 	}
 }
