28664c4d2b66e3c79787dd9403c25f5f8a3cc7f1
Kingchang25 <lachang5711@gmail.com>
2026-09-09 18:12:32 +0000

    update

--- Java/102_Exercises/08_ErrorPractice/error.java
+++ Java/102_Exercises/08_ErrorPractice/error.java
@@ -1,18 +1,18 @@
 class error {
 	public static void main(String args[]) {
         
-		system.out.println"Happy vacation day back!";
-        system.out.println"What'd you do this weekend?
+		System.out.println("Happy vacation day back!");
+        System.out.println("What'd you do this weekend?
                            I watched a movie!";
 
-		int countnumber = 125;
+		int count = 125;
 		System.out.println(count);
 
 		int num1 = 12.2;
 		int num2 = 14.3;
-		answer = num1 * num2 + answer;
+		double answer = num1 * num2 + answer;
 		
-		System.out.println(answer)
+		System.out.println(answer);
 
 		System.out.println("I now have 0 errors!!!");		
 	}
