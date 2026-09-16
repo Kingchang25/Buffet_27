5f62d94a8b02aad4a75c58165c8f46a79e877a95
Kingchang25 <lachang5711@gmail.com>
2026-09-16 18:39:13 +0000

    commit and sync

--- Java/000_Labs/009_math_methods/base_code/starter.java
+++ Java/000_Labs/009_math_methods/base_code/starter.java
@@ -1,15 +1,20 @@
 /*
- *	Author:  
- *  Date: 
+ *	Author:  Luke Castillote Chang
+ *  Date: 9/16/26
 */
 
 import java.util.Scanner;
 
 class starter {
 	public static void main(String args[]) {
-		
-		// the string "I love to learn coding remotely." will appear in
-		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		 int maxValue = Math.max(-8, -4);
+        double sqrtValue = Math.sqrt(27);
+        double powerValue = Math.pow(2.5, 18);
+        double maxDouble = Math.max(4.0, 3.5);
+
+        System.out.println("Maximum = " + maxValue);
+        System.out.println("Square root = " + sqrtValue);
+        System.out.println("Power = " + powerValue);
+        System.out.println("Max = " + maxDouble);
 	}
 }
