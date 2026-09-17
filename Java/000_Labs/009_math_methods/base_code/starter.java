97b12a7f691411dd516baf2d28708a5c7e6a172d
Kingchang25 <lachang5711@gmail.com>
2026-09-17 18:32:06 +0000

    update math calculations in starter.java

--- Java/000_Labs/009_math_methods/base_code/starter.java
+++ Java/000_Labs/009_math_methods/base_code/starter.java
@@ -8,13 +8,13 @@ import java.util.Scanner;
 class starter {
 	public static void main(String args[]) {
 		 int maxValue = Math.max(-8, -4);
-        double sqrtValue = Math.sqrt(27);
-        double powerValue = Math.pow(2.5, 18);
-        double maxDouble = Math.max(4.0, 3.5);
+        double sqrtValue = Math.sqrt(3*8+31%7);
+        double powerValue = Math.pow((37/3), (35%21) );
+        double maxDouble = Math.max(Math.pow(2,14%3), Math.sqrt(2*6));
 
         System.out.println("Maximum = " + maxValue);
         System.out.println("Square root = " + sqrtValue);
         System.out.println("Power = " + powerValue);
-        System.out.println("Max = " + maxDouble);
+        System.out.println("Max double = " + maxDouble);
 	}
 }
