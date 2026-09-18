16043703be825d92d2bfadaa4bfee709d5c6ede5
Kingchang25 <lachang5711@gmail.com>
2026-09-18 17:54:45 +0000

    commit and sync these new changes

--- Java/000_Labs/010_random_method-static/base_code/starter.java
+++ Java/000_Labs/010_random_method-static/base_code/starter.java
@@ -8,8 +8,14 @@ import java.util.Random;
 
 class starter {
 	public static void main(String args[]) {
-		// the string "I love to learn coding remotely." will appear in
-		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		int gamble1 = (int)(Math.random()* 9 );
+		int gamble2 = (int)(Math.random()* 10 + 1 );
+		double gamble3 = (Math.random()* 1.0 +2.5 );
+		System.out.print("A number between 0 and 9 is: ");
+		System.out.println(gamble1);
+		System.out.print("A number between 1 and 10 is: ");
+		System.out.println(gamble2);
+		System.out.print("A number between 2.5 and 3.5 is : ");
+		System.out.println(gamble3);
 	}
 }
