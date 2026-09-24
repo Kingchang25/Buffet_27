7101e2d1e5f15c305514cd17db6c0f9cdfa5eaf7
Kingchang25 <lachang5711@gmail.com>
2026-09-24 18:13:40 +0000

    update

--- Java/000_Labs/010_random_method-static/base_code/starter.java
+++ Java/000_Labs/010_random_method-static/base_code/starter.java
@@ -11,12 +11,13 @@ class starter {
 		int gamble1 = (int)(Math.random()* 9 );
 		int gamble2 = (int)(Math.random()* 10 + 1 );
 		double gamble3 = (Math.random()* 1.0 +2.5 );
+		double gamble4 = (Math.random()* 589 + 15 );
 		System.out.print("A number between 0 and 9 is: ");
 		System.out.println(gamble1);
 		System.out.print("A number between 1 and 10 is: ");
 		System.out.println(gamble2);
 		System.out.print("A number between 2.5 and 3.5 is : ");
 		System.out.println(gamble3);
-		System.out.println("A double between 14 and 589: " +  )
+		System.out.println("A double between 14 and 589: " + gamble4);
 	}
 }
--- Java/000_Labs/011_if_statements/base_code/starter.java
+++ Java/000_Labs/011_if_statements/base_code/starter.java
@@ -1,14 +1,19 @@
 /*
- *	Author:  
- *  Date: 
+ *	Author: Luke Castillote Chang
+ *  Date: 9/22/26
 */
 
 import java.util.Scanner;
 
 class starter {
 	public static void main(String args[]) {
-		// the string "I love to learn coding remotely." will appear in
-		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		int numero1 =5;
+		int numero2 =12;
+		Boolean answer = numero1<numero2;
+		if(answer) {
+			System.out.println("the first variable is: 7");
+			System.out.println("the second variable is: 12");
+			System.out.println("The variables are different");
+		}
 	}
 }
